fc480e032dad21fb17c4388b604a767b8f62335a
Vitor Silva Lima <vitor.lima2@fatec.sp.gov.br>
2015-11-16 16:27:48 -0200

    First commit

---
+++ src/main/webapp/js/App.js
@@ -0,0 +1,34 @@
+var app = angular.module("VivoApp",['ngRoute',]).config(
+        [ '$httpProvider', '$routeProvider', function($httpProvider, $routeProvider) {
+            /*
+             * Use a HTTP interceptor to add a nonce to every request to prevent MSIE from caching responses.
+             */
+            $httpProvider.interceptors.push('ajaxNonceInterceptor');
+
+            $routeProvider.
+            // if URL fragment is /home, then load the home partial, with the
+            // MembersCtrl controller
+            when('/login', {
+                templateUrl : 'templates/login.html',
+                //controller : MembersCtrl
+            // Add a default route
+            }).otherwise({
+                redirectTo : '/login'
+            });
+        } ])
+    .factory('ajaxNonceInterceptor', function() {
+        // This interceptor is equivalent to the behavior induced by $.ajaxSetup({cache:false});
+
+        var param_start = /\?/;
+
+        return {
+
+            request : function(config) {
+                if (config.method == 'GET') {
+                    // Add a query parameter named '_' to the URL, with a value equal to the current timestamp
+                    config.url += (param_start.test(config.url) ? "&" : "?") + '_=' + new Date().getTime();
+                }
+                return config;
+            }
+        }
+    });
